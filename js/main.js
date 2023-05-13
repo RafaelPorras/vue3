37d602c07eb8ebe5b7bf3bad1610260e8bcf3053
rporras <rporras@aibesoft.com>
2023-05-13 10:48:40 +0200

    project structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 const API = "https://api.github.com/users/";
+
+
+
+const app = Vue.createApp({
+    data(){
+        return{
+          search:null,
+          result:null,
+          error:null,
+        }
+    },
+    methods:{
+        async doSearch(){
+            this.result = this.error = null;
+
+            try {
+                const response = await fetch(API + this.search);
+
+                if(!response.ok) throw new Error("User not found");
+
+                const data = await response.json()
+                this.result=true; 
+            } 
+            catch (error) {
+                this.error = error;
+            }
+            finally{
+                this.search = null;
+            }
+        }
+    }
+});
+
